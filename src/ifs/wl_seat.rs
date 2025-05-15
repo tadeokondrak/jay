209e036c3850caf11fc60015c4d6a2642dbbe870
Tadeo Kondrak <me@tadeo.ca>
2025-05-14 18:14:51 -0600

    use keyboard focused node for new windows

--- src/ifs/wl_seat.rs
+++ src/ifs/wl_seat.rs
@@ -433,6 +433,9 @@ impl WlSeatGlobal {
     }
 
     pub fn get_output(&self) -> Rc<OutputNode> {
+        if let Some(output) = self.get_keyboard_output() {
+            return output;
+        }
         self.cursor_user_group.latest_output()
     }
 
